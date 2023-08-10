61a021d33e1260344225c0c2a6d13f65a2c62cb9
RuiqiWang00 <1965448834@qq.com>
2023-08-10 15:46:28 +0800

    update

--- config/run_eval_camera.py
+++ config/run_eval_camera.py
@@ -2,5 +2,3 @@ _base_=['run.py']
 train=False
 DATA='val'
 RESUME_FILE = 'checkpoint_epoch_50.tar.pth'
-MODEL['decoder']['training']=False
-MODEL['training']=False
--- config/run_eval_real.py
+++ config/run_eval_real.py
@@ -2,5 +2,3 @@ _base_=['run.py']
 train=False
 DATA='real_test'
 RESUME_FILE = 'checkpoint_epoch_50.tar.pth'
-MODEL['decoder']['training']=False
-MODEL['training']=False
